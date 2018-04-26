e0210e11bde943aab177f90d225816311a496cfa
Fasion Chan <fasionchan@gmail.com>
2018-04-26 08:22:46 +0800

    add script print-dmi-jsonic.py

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # dmi-python
+
+```
+>>> from dmi import fetch_dmi
+>>> for item in fetch_dmi():
+...     print(item)
+```
+
+```
+>>> from dmi import print_dmi_jsonic
+>>> print_dmi_jsonic()
+```
---
+++ bin/print-dmi-jsonic.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- encoding=utf8 -*-
+
+'''
+FileName:   print-dmi-jsonic.py
+Author:     Fasion Chan
+@contact:   fasionchan@gmail.com
+@version:   $Id$
+
+Description:
+
+Changelog:
+
+'''
+
+from dmi import print_dmi_jsonic
+
+print_dmi_jsonic()
